655208ebfc6487b4f51c97e50f7b7f1f50570491
thupten tsundue <thups1951@gmail.com>
2024-12-05 12:22:25 -0500

    added oder route and fuction to create and fetch oder

--- routes/Oders.js
+++ routes/Oders.js
@@ -3,6 +3,7 @@ const oderRoute = express.Router();
 const AsynHandler = require("express-async-handler");
 const OderItem = require("../model/Oder");
 const protect = require("../middleware/auth");
+const Oder = require("../model/Oder");
 
 oderRoute.post(
   "/",
@@ -36,6 +37,56 @@ oderRoute.post(
 
       const createdOder = await oder.save();
       res.status(200).send(createdOder);
+    }
+  })
+);
+
+oderRoute.put(
+  "/:id/payment",
+  protect,
+  AsynHandler(async (req, res) => {
+    const oder = await Oder.findById(req.params.id);
+    if (oder) {
+      (oder.isPaid = true),
+        (oder.paidAt = Date.now()),
+        (oder.paymentResult = {
+          id: req.body.id,
+          status: req.body.status,
+          updateed_time: req.body.updateed_time,
+          email_address: req.body.email_address,
+        });
+      const updatedOder = await oder.save();
+      res.send(updatedOder);
+    } else {
+      res.status(400);
+      throw new Error(" No oder found");
+    }
+  })
+);
+
+oderRoute.get(
+  "/",
+  protect,
+  AsynHandler(async (req, res) => {
+    const oder = await Oder.find({ user: req.user._id }).sort({ _id: -1 });
+    if (oder) {
+      res.status(200).send(oder);
+    } else {
+      res.status(400);
+      throw new Error("no oder found");
+    }
+  })
+);
+oderRoute.get(
+  "/:id",
+  protect,
+  AsynHandler(async (req, res) => {
+    const oder = await Oder.findById(req.params.id).populate("user", "email");
+    if (oder) {
+      res.status(200).send(oder);
+    } else {
+      res.status(400);
+      throw new Error("no oder found");
     }
   })
 );
